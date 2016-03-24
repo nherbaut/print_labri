51e6faabe73fe397ed9ef300cd21528af94c47a7
Nicolas Herbaut <nherbaut@labri.fr>
2016-03-24 10:00:27 +0100

    added 2 sided option

--- print_labri.sh
+++ print_labri.sh
@@ -17,6 +17,9 @@ select opt in "${options[@]}" "Quit" ; do
     fi
 done    
 
+
+-o sides=two-sided-long-edge
+
 prompt="Please select a file:"
 options=( $(find -maxdepth 1 -type f -name "*.pdf"  -print0| xargs -0|grep "\.pdf") )
 PS3="$prompt "
@@ -26,11 +29,38 @@ select file in "${options[@]}" "Quit" ; do
 
     elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
         echo  "You picked $file which is file $REPLY"
-        scp $file $LABRI_SERVER:.
-        ssh -o "BatchMode yes" ssh.labri.fr "lpr -P $opt $file"
         break
 
     else
         echo "Invalid option. Try another one."
     fi
 done    
+
+
+prompt="Two sided?"
+options=( $(echo "Y N"|xargs -0) )
+PS3="$prompt "
+select twosided in "${options[@]}" "Quit" ; do 
+    if (( REPLY == 1 + ${#options[@]} )) ; then
+        exit
+
+    elif (( REPLY > 0 && REPLY <= ${#options[@]} )) ; then
+		echo "file is beeing copied to remote server"
+        scp -q $file $LABRI_SERVER:. 
+        echo "file copied"
+        echo "printing launched"
+        if [ "$twosided" = "Y" ]; then
+			ssh -o "BatchMode yes" ssh.labri.fr "lpr -P $opt $file -o media=A4 -o sides=two-sided-long-edge"
+		else
+			ssh -o "BatchMode yes" ssh.labri.fr "lpr -P $opt $file -o media=A4"
+		fi
+			echo "File removed from remote server. Now go get your dead trees."
+			ssh -o "BatchMode yes" ssh.labri.fr "rm $file"
+        break
+
+    else
+        echo "Invalid option. Try another one."
+    fi
+done   
+
+
